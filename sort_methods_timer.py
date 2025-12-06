536153fb0e7155118bc22a1617b84c0b232d4a40
Natalya <nidnatasha@gmail.com>
2025-12-06 11:04:59 +0000

    Added conclusions and plots

--- sort_methods_timer.py
+++ sort_methods_timer.py
@@ -3,77 +3,87 @@ import timeit
 import matplotlib.pyplot as plt
 
 
-def insertion_sort(l, left=0, right=None):
+def insertion_sort(arr, left=0, right=None):
     if right is None:
-        right = len(l) - 1
+        right = len(arr) - 1
 
     for i in range(left+1, right+1):
-        key = l[i]
-        j = i-1
-        while j >= left and l[j] > key:
-            l[j+1] = l[j]
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
             j -= 1
-        l[j+1] = key
-    return l
+        arr[j + 1] = key
+    return arr
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
+def mergesort(arr):
+    if len(arr) < 2:
         return arr
 
     mid = len(arr) // 2
-    left_half = arr[:mid]
-    right_half = arr[mid:]
+    left = arr[:mid]
+    right = arr[mid:]
 
-    return merge(merge_sort(left_half), merge_sort(right_half))
+    return merge_s(
+        mergesort(left), 
+        mergesort(right))
 
 
-def merge(left, right):
-    merged = []
-    left_index = 0
-    right_index = 0
+def merge_s(left, right):
+    if not left:
+        return right
+    
+    if not right:
+        return left
+    
+    result = []
+    left_index = right_index = 0
 
     # Спочатку об'єднайте менші елементи
-    while left_index < len(left) and right_index < len(right):
+    while len(result) < len(left) + len(right):
         if left[left_index] <= right[right_index]:
-            merged.append(left[left_index])
+            result.append(left[left_index])
             left_index += 1
         else:
-            merged.append(right[right_index])
+            result.append(right[right_index])
             right_index += 1
 
-    # Якщо в лівій або правій половині залишилися елементи,
-	# додайте їх до результату
-    while left_index < len(left):
-        merged.append(left[left_index])
-        left_index += 1
+        # Якщо в лівій або правій половині залишилися елементи,
+        # додайте їх до результату
+        if right_index == len(right):
+            result += left[left_index:]
+            break
 
-    while right_index < len(right):
-        merged.append(right[right_index])
-        right_index += 1
+        if left_index == len(left):
+            result += right[right_index:]
+            break
+    
+    return result
 
-    return merged
 
-
-def tim_sort(l):
+def tim_sort(arr):
     min_run = 32
-    n = len(l)
+    n = len(arr)
 
     for i in range(0, n, min_run):
-        insertion_sort(l, i, min((i+min_run-1), (n-1)))
+        insertion_sort(arr, i, min(i + min_run - 1, (n - 1)))
 
-        size = min_run
-        while size < n:
-            for s in range(0, n, size*2):
-                mid = s + size-1
-                end = min((s+size*2-1), (n-1))
+    size = min_run
+    while size < n:
+        for start in range(0, n, size * 2):
+            mid = start + size
+            end = min((start + size * 2 - 1), (n - 1))
 
-                merged = merge(left=l[s:mid+1], right=l[mid+1:end+1])
+            merged_array = merge_s(
+                left = arr[start:mid], 
+                right = arr[mid:end + 1]
+                )
 
-                l[s:s+len(merged)] = merged
+            arr[start:start+len(merged_array)] = merged_array
 
         size *= 2
-    return l
+    return arr
 
 
 def random_number(num):
@@ -83,87 +93,110 @@ def random_number(num):
 
     return lst_random
 
-# [random.randint(0, 10) for _ in range(5)]
-        
 
+def timer_measurement(arr, sort_function_name):
+    # Створюємо словник globals, щоб timeit міг бачити функцію сортування 
+    # та масив (як 'arr_to_sort')
+    setup_globals = {
+        'sort_func': sort_function_name,
+        'arr_to_sort': arr
+    }
+    stmt = "sort_func(arr_to_sort.copy())"
+    sort_time = timeit.timeit(
+        stmt=stmt, 
+        globals=setup_globals,
+        number=10
+    )
+    return sort_time / 10
+
+
+def pythont_standart_sort(arr):
+    arr.sort()
+    return arr
 
 if __name__ == '__main__':
-    # numbers = [random.randint(0, 10) for _ in range(5)] # 5-10-15-20-25-30-35-40-45-50-55-60-65-70-75-80-85-90-95-100
-
-    # print("Hello")
-    insertion_sort_lst = []
+    print("HELLO")
+    # insertion_sort_lst = []
     merge_sort_lst = []
     tim_sort_lst = []
-    x_axis = []
-
-    for i in range(10, 100, 10):
+    pythont_standart_sort_lst = []
+    x_axis = []        
+    
+    for i in range(10, 100000, 10000):
+        print(f"******************** {i} ************************")
         x_axis.append(i)
 
-        insertion_sort_time = timeit.timeit(
-            f"insertion_sort({random_number(i)})", setup="from __main__ import insertion_sort")
-        insertion_sort_lst.append(insertion_sort_time)
+        arr = random_number(i)
 
-        merge_sort_time = timeit.timeit(
-            f"merge_sort({random_number(i)})", setup="from __main__ import merge_sort")
-        merge_sort_lst.append(merge_sort_time)
+        default_s = timer_measurement(arr, pythont_standart_sort)
+        print(f"Default sort: {default_s:.6f} sec")
+        pythont_standart_sort_lst.append(default_s)
 
-        tim_sort_time = timeit.timeit(
-            f"tim_sort({random_number(5)})", setup="from __main__ import tim_sort")
-        tim_sort_lst.append(tim_sort_time)
+        # insertion_s = timer_measurement(arr, insertion_sort)
+        # print(f"Insertion: {insertion_s:.6f} sec")
+        # insertion_sort_lst.append(insertion_s)
+        
+        # Merge Sort (O(n log n))
+        merge_st = timer_measurement(arr, mergesort)
+        print(f"Merge: {merge_st:.6f} sec") 
+        merge_sort_lst.append(merge_st)
 
-# Налаштування розміру фігури
-plt.figure(figsize=(10, 6))
+        # Timsort (O(n log n))
+        tim_s = timer_measurement(arr, tim_sort)
+        print(f"Timsort: {tim_s:.6f} sec")
+        tim_sort_lst.append(tim_s)
 
-# # Дані для осі X (спільні для всіх ліній)
-# x_data = x_axis
+    # Налаштування розміру фігури
+    plt.figure(figsize=(12, 7))
 
-# # Дані для insertion_sort
-# y1_data = insertion_sort_lst
+    # # Лінія для insertion_sort
+    # plt.plot(
+    #     x_axis,
+    #     insertion_sort_lst,
+    #     marker='o',
+    #     linestyle='-',
+    #     color='red',
+    #     label='Insertion sort'
+    # )
 
-# # Дані для merge_sort
-# y2_data = merge_sort_lst
+    # Лінія для merge_sort
+    plt.plot(
+        x_axis,
+        merge_sort_lst,
+        marker='x',
+        linestyle='--',
+        color='blue',
+        label='Merge sort'
+    )
 
-# # Дані для tim_sort
-# y3_data = tim_sort_lst
+    # Лінія для tim_sort
+    plt.plot(
+        x_axis,
+        tim_sort_lst,
+        marker='+',
+        linestyle='--',
+        color='green',
+        label='Timsort'
+    )
+    
+    # Лінія для pythont_standart_sort
+    plt.plot(
+        x_axis,
+        pythont_standart_sort_lst,
+        marker='o',
+        linestyle='--',
+        color='black',
+        label='Pythont Standart Sort'
+    )
 
-# Лінія для insertion_sort
-plt.plot(
-    x_axis,
-    insertion_sort_lst,
-    marker='o',
-    linestyle='-',
-    color='red',
-    label='Insertion sort'
-)
+    plt.xlabel('Array Length', fontsize=12)
+    plt.ylabel('Sort Time (seconds)', fontsize=12)
 
-# Лінія для merge_sort
-plt.plot(
-    x_axis,
-    merge_sort_lst,
-    marker='x',
-    linestyle='--',
-    color='blue',
-    label='Merge sort'
-)
+    # Додавання легенди є КЛЮЧОВИМ для декількох ліній
+    plt.legend(loc='upper left') 
 
-# Лінія для tim_sort
-plt.plot(
-    x_axis,
-    tim_sort_lst,
-    marker='+',
-    linestyle='--',
-    color='blue',
-    label='Timsort'
-)
+    plt.xticks(x_axis) 
+    plt.grid(True, linestyle=':', alpha=0.6) 
 
-plt.xlabel('Array Length', fontsize=12)
-plt.ylabel('Sort Time', fontsize=12)
-
-# Додавання легенди є КЛЮЧОВИМ для декількох ліній
-plt.legend(loc='upper left') 
-
-plt.xticks(x_axis) 
-plt.grid(True, linestyle=':', alpha=0.6) 
-
-# Відображення графіка
-plt.show()
+    # Відображення графіка
+    plt.show()
